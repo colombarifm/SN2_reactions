222b7076ae226d80c23f6b1e774e82f14cb8dd17
felippe <colombarifm@hotmail.com>
2026-09-07 13:47:08 -0300

    fix 00_relaxed_scan/run_scan.sh slurm things

--- 00_relaxed_scan/run_scan.sh
+++ 00_relaxed_scan/run_scan.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=sn2_scan
 #SBATCH --partition=cpu
-#SBATCH --timelimit=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --mem=4G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -11,6 +11,6 @@ module load ORCA/6.1.1
 
 export OMP_NUM_THREADS=${SLURM_NTASKS_PER_CORE}
 
-${orca} scan.inp > scan.out
+orca scan.inp > scan.out
 
-grep -v ">" scan.allxyz > scal_final.xyz
+grep -v ">" scan.allxyz > scan_final.xyz
