349b1b7cc807bd062597ff906b2f3a698cb2c407
felippe <colombarifm@hotmail.com>
2026-09-07 15:34:31 -0300

    add 02_IRC directory

---
+++ 02_IRC/run_irc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=irc
+#SBATCH --partition=cpu
+#SBATCH --time=00:10:00
+#SBATCH --mem=4G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+module load ORCA/6.1.1
+
+export OMP_NUM_THREADS=${SLURM_NTASKS_PER_CORE}
+
+orca irc.inp > irc.out
